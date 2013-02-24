59099a0f4a3fee1e9d3705a61d0fdead83fe7fba
Jackson Owens <jackson_owens@brown.edu>
2013-02-23 23:27:41 -0500

    lol

--- src/server.js
+++ src/server.js
@@ -1,3 +1,16 @@
 /**
  * Food trucks, brah.
  */
+"use strict";
+
+var express = require("express");
+
+var app = express();
+
+var port = 8080;
+
+app.use("/public", express.static(__dirname + '/public'));
+
+app.listen(port, function () {
+    console.log('- Server listening on port ' + port);
+});
