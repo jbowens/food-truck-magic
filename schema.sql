21ed04bc1d64d8bb32594a22f15c4d2e97822cdd
Jackson Owens <jackson_owens@brown.edu>
2013-03-02 22:57:42 -0500

    fixed schema.sql some

--- schema.sql
+++ schema.sql
@@ -1,4 +1,10 @@
-CREATE TABLE trucks (
+CREATE TABLE IF NOT EXISTS locations (
+    locationid SERIAL PRIMARY KEY,
+    geoPoint GEOGRAPHY(Point),
+    textLoc VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS trucks (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     twitterName VARCHAR(255),
@@ -9,25 +15,19 @@ CREATE TABLE trucks (
     currLocationId INTEGER NOT NULL REFERENCES locations(locationid)
 );
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL,
     pass CHAR(256) NOT NULL,
     email VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE vendors (
+CREATE TABLE IF NOT EXISTS vendors (
     userid INTEGER NOT NULL REFERENCES users(id),
     truckid INTEGER NOT NULL REFERENCES trucks(id)
 );
 
-CREATE TABLE locations (
-    locationid SERIAL PRIMARY KEY,
-    geoPoint GEOGRAPHY(Point),
-    textLoc VARCHAR(255)
-);
-
-CREATE TABLE follows (
+CREATE TABLE IF NOT EXISTS follows (
     userid INTEGER NOT NULL REFERENCES users(id),
     truckid INTEGER NOT NULL REFERENCES trucks(id)
 );
