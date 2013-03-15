ccd8c808c2e33306b6f2155f5f521ac03c2924f5
Jackson Owens <jackson_owens@brown.edu>
2013-03-14 22:24:48 -0400

    added 404 catchall

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,17 @@
  * Routes homey.
  */
 
+function fourOhFourRoute(request, response) {
+    response.status(404);
+    response.render('fourOhFour', {});
+}
+
+
 exports.setupRoutes = function(app) {
     app.get('/', function(request, response) {
         response.render('index', {test : 'yolo'});
     });
+
+    app.get('*', fourOhFourRoute);
+    app.post('*', fourOhFourRoute);
 };
