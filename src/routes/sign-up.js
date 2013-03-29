33c77bc9c30b9bfd7df6c5eebb7d9f46366ae571
Jackson Owens <jackson_owens@brown.edu>
2013-03-28 21:30:49 -0400

    added username validation to /sign-up server side

--- src/routes/sign-up.js
+++ src/routes/sign-up.js
@@ -1,15 +1,19 @@
 /*
  * The route for the /sign-up page.
  */
+var _ = require('underscore');
 var db = require('../db.js').Database;
 var fourOhFourRoute = require('./fourohfour.js').route;
+var checkUsername = require('./api/check-username.js').checkUsername;
 
 /* SQL Queries */
 var SQL_CHECK_USERNAME = "SELECT id FROM users WHERE name LIKE $1 LIMIT 1";
 
 var defaultTemplateData = {
     noUsername: false,
-    noPassword: false
+    noPassword: false,
+    usernameTaken: false,
+    enteredUsername: null
 };
 
 exports.route = function(request, response) {
@@ -17,7 +21,7 @@ exports.route = function(request, response) {
 };
 
 exports.postRoute = function(request, response) {
-    var data = defaultTemplateData;
+    var data = _.clone(defaultTemplateData);
     var err = false;
 
     if(!request.body.name) {
@@ -30,14 +34,34 @@ exports.postRoute = function(request, response) {
         data.noPassword = true;
     }
 
-    /* TODO: Validate username */
-    /* TODO: Create user */
-    /* TODO: Set user session to log them in */
-    /* TODO: Send an email to the user? */
+    data.enteredUsername = request.body.name;
 
-    if(err) {
-        response.render('sign-up', data);
-    } else {
-        /* TODO: Redirect */
-    }
+    // TODO: Do we care enough to check if the username is taken
+    // in the same transaction as creating the user?
+
+    checkUsername(request.body.name, function(error, taken) {
+        
+        if(error) {
+            console.error(error);
+            // TODO: Figure out what to do here. Print an
+            // error message to the user?
+        }
+
+        if(taken) {
+            err = true;
+            data.usernameTaken = true;
+        }
+ 
+        /* TODO: Create user */
+        /* TODO: Set user session to log them in */
+        /* TODO: Send an email to the user? */
+
+        if(err) {
+            response.render('sign-up', data);
+        } else {
+            /* TODO: Redirect */
+        }
+
+    });
+ 
 };
