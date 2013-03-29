445358466da2d7fd1b55739351b36dd6cef87141
Jackson Owens <jackson_owens@brown.edu>
2013-03-28 21:14:38 -0400

    checked check-username api to also export a function for checking if a username is taken

--- src/routes/api/check-username.js
+++ src/routes/api/check-username.js
@@ -8,6 +8,32 @@ var fourOhFourRoute = require('../fourohfour.js').route;
 /* SQL Queries */
 var SQL_CHECK_USERNAME = "SELECT id FROM users WHERE name LIKE $1 LIMIT 1";
 
+/* Checks if the given username is in use.
+ */
+exports.checkUsername = function(username, callback) {
+
+    db.get(function(err, conn) {
+        if(err) {
+            callback(err, null);
+        }
+
+        conn.query(SQL_CHECK_USERNAME, [username], function(err, res) {
+            db.release(conn);
+
+            if(err) {
+                callback(err, null);
+            }
+
+            var userExists = !! res.rows.length;
+
+            callback(null, userExists);
+
+        });
+
+    });
+
+};
+
 exports.route = function(request, response) {
 
     if(!request.params.username) {
@@ -15,27 +41,15 @@ exports.route = function(request, response) {
         return fourOhFourRoute(request, response);
     } 
 
-    db.get(function(err, conn) {
-        if(err) {
-            // Couldn't get a database connection.
-            console.error(err);
-            return fourOhFourRoute(request, response);
-        }
+    exports.checkUsername(request.params.username,
+            function(err, usernameExists) {
+                if(err) {
+                    console.error(err);
+                    fourOhFourRoute(request, response);
+                } else {
+                    response.json({'username_exists': usernameExists});
+                }
 
-        conn.query(SQL_CHECK_USERNAME, [request.params.username], function(err, res) {
-            db.release(conn);
-
-            if(err) {
-                console.error(err);
-                return fourOhFourRoute(request, response);
-            }
-
-            var userExists = !! res.rows.length;
-
-            response.json({'username_exists': userExists});
-
-        });
-
-    });
+            });
 
 };
