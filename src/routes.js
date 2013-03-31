ccd5fa7a5aa780de4bf729586482db8d66800717
Jackson Owens <jackson_owens@brown.edu>
2013-03-31 12:29:45 -0400

    login page

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,8 @@ exports.setupRoutes = function(app) {
     app.get('/api/check-username/:username', require('./routes/api/check-username.js').route);
     app.post('/sign-up', require('./routes/sign-up.js').postRoute);
     app.get('/sign-up', require('./routes/sign-up.js').route);
+    app.post('/login', require('./routes/login.js').postRoute);
+    app.get('/login', require('./routes/login.js').route);
     app.get('/truck/:truckidentifier', require('./routes/truck.js').route);
     app.get('*', require('./routes/fourohfour.js').route);
     app.post('*', require('./routes/fourohfour.js').route);
---
+++ src/routes/login.js
@@ -0,0 +1,100 @@
+/*
+ * The route for the /login page.
+ */
+var _ = require('underscore');
+var db = require('../db.js').Database;
+var hash = require('../hasher.js').hash;
+var sanitize = require('validator').sanitize;
+var bailout = require('./fatalerror.js').bailout;
+
+/* SQL Queries */
+var SQL_GET_USER = 'SELECT * FROM users WHERE name = $1 AND pass = $2';
+
+function getUser(username, password, callback) {
+
+    var hashedPassword = hash(password);
+
+    db.get(function(err, conn) {
+        if(err) {
+            return callback(err, null);
+        }
+
+        conn.query(SQL_GET_USER, [username, hashedPassword], function(err, res) {
+            db.release(conn);
+            
+            if(err) {
+                console.error(err);
+                callback(err, null);
+            }
+
+            console.log(res);
+
+            if(res && res.rowCount) {
+                callback(null, res.rows[0]);
+            } else {
+                callback(null, null);
+            }
+
+        });
+    });
+
+}
+
+/*
+ * Fuck this templating engine.
+ */
+var defaultTemplateData = {
+    badLoginCredentials: false,
+    enteredUsername: ''
+};
+
+/*
+ * Handles actually displaying the page. This function takes in
+ * the template data that should be passed on to the templating
+ * engine.
+ */
+function route(request, response, data) {
+    console.log('Displaying login page with data:', data);
+    response.render('login', data);
+}
+
+/*
+ * The route for normal get requests to the login page.
+ */
+exports.route = function(request, response) {
+    route(request, response, defaultTemplateData);
+};
+
+exports.postRoute = function(request, response) {
+    var data = _.clone(defaultTemplateData);
+    var err = false;
+    
+    if(!request.body.name || !request.body.pass) {
+        err = true;
+        data.badLoginCredentials = true;
+    } else {
+        request.body.name = sanitize(request.body.name).trim();
+        data.enteredUsername = request.body.name;
+    }
+
+    if(err) {
+        return route(request, response, data);
+    }
+
+    // Try and get the user from the database
+    getUser(request.body.name, request.body.pass, function(err, user) {
+        
+        if(!user) {
+            err = true;
+            data.badLoginCredentials = true;
+            // Re-display the login page
+            return route(request, response, data);
+        } else {
+            // Legitimate login
+            request.session.userid = user.id;
+            response.redirect('/');
+        }
+
+    });
+
+};
