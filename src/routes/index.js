afc354070956802e1f4efb9d6989eff29dc010f4
Alec Lee <alec.lee15@gmail.com>
2013-03-31 22:27:18 -0400

    index now shows username yay

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,18 @@
 var userStore = require('../userstore.js').UserStore;
+var _ = require('underscore');
+
+var defaultTemplateData = {
+    user: null
+};
 
 exports.route = function(request, response) {
-
-    var data = {test : 'yolo'};
+    var data = _.clone(defaultTemplateData);
 
     function renderPage() {
         response.render('index', data);
     }
 
-    if(request.session.userid) {
+    if (request.session.userid) {
         userStore.getById(request.session.userid, function(user) {
             data.user = user;
             console.log(user);
