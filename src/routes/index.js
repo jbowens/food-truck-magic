322fa67856d8e0820595bbf55c17f2fce494c2b3
Alec Lee <alec.lee15@gmail.com>
2013-04-06 19:52:57 -0400

    show followed trucks on the trucks page now!

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,8 @@ var _ = require('underscore');
 var db = require('../db.js').Database;
 var bailout = require('./fatalerror.js').bailout;
 
-var SQL_GET_FOLLOWS = 'SELECT * FROM follows WHERE userid=$1';
-
 var defaultTemplateData = {
     user: null,
-    following: null
 };
 
 
@@ -16,30 +13,8 @@ exports.route = function(request, response) {
         response.render('index', data);
     }
 
-
     if (request.session.user) {
         data.user = request.session.user;
-
-        /* let's get the list of following trucks */
-        db.get(function(err, conn) {
-            if(err) {
-                bailout(request, response, err);
-            }
-
-            conn.query(SQL_GET_FOLLOWS, [request.session.user.id], function(err, res) {
-                db.release(conn);
-                if (err) {
-                    renderPage();
-                }
-
-                if (res && res.rows) {
-                    data.following = res.rows;
-                }
-                renderPage();
-            });
-        });
-    } else {
-        /* not logged in */
-        renderPage();
     }
+    renderPage();
 };
--- src/routes/trucks.js
+++ src/routes/trucks.js
@@ -1,35 +1,46 @@
 /*
  * The route for the aggregate list of all trucks
- * TODO: For now, just going to show every single truck. we 
- * are going to have to be smarter about this later on.
  */
 
+var _ = require('underscore');
 var db = require('../db.js').Database;
 var fourOhFourRoute = require('./fourohfour.js').route;
 
 /* SQL Queries */
 var SQL_GET_TRUCKS = 'SELECT * FROM trucks';
+var SQL_GET_FOLLOWED = 'SELECT trucks.* FROM follows INNER JOIN trucks on trucks.id = follows.truckid WHERE userid = $1';
+
+
+var defaultTemplateData = {
+    user: null,
+    allTrucks: [],
+    followedTrucks: [],
+};
 
 exports.route = function(request, response) { 
-    db.get(function(err, conn) {
+    var data = _.clone(defaultTemplateData);
+    data.user = request.session.user;
+    
+    /* query for all trucks */
+    db.query(SQL_GET_TRUCKS, [], function(err, res) {
         if (err) {
             console.error(err);
-            return fourOhFourRoute(request, response);
+            return fourOhFourRuote(request, response);
         }
+        data.allTrucks = res.rows;
 
-        conn.query(SQL_GET_TRUCKS, function(err, res) {
-            db.release(conn);
-            var trucks = res.rows;
-
-            /* if the sql query somehow messes up */
-            if (err || !res.rows.length) {
-                if(err) {
+        /* if logged in, query for trucks user follows */
+        if (data.user) {
+            db.query(SQL_GET_FOLLOWED, [data.user.id], function(err, res) {
+                if (err) {
                     console.error(err);
+                    return fourOhFourRuote(request, response);
                 }
-                return fourOhFourRoute(request, response);
-            }
-
-            response.render('trucks',  {'trucks' : trucks});
-        });
+                data.followedTrucks = res.rows;
+                response.render('trucks', data);
+            });
+        } else {
+            response.render('trucks', data);
+        }
     });
 };
