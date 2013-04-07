ff3046a408c0ef3888cf234578db87105c2742be
Alec Lee <alec.lee15@gmail.com>
2013-04-07 13:31:09 -0400

    added swiggg

--- src/server.js
+++ src/server.js
@@ -6,7 +6,8 @@
 var SESSION_SECRET_KEY = 'ft_session';
 
 var express = require('express');
-var engine = require('ejs-locals');
+var cons = require('consolidate');
+var swig = require('swig');
 var args = require('optimist').argv;
 
 var db = require('./db.js').Database;
@@ -26,8 +27,8 @@ app.configure(function() {
         store: new express.session.MemoryStore({reapInterval: 30000})
     }));
 
-    app.engine('ejs', engine);      // use ejs-locals for ejs templates
-    app.set('view engine', 'ejs');
+    app.engine('.html', cons.swig);
+    app.set('view engine', 'html');
     app.set('views', __dirname + '/views');
     app.use(express.static(__dirname +'/public'));
 });
