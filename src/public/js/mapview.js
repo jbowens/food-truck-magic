c4bc7a1c781b8de2a5f503bb6102eca56ed979c8
Alec Lee <alec.lee15@gmail.com>
2013-04-09 20:50:31 -0400

    marker on that truckler map

--- src/public/js/mapview.js
+++ src/public/js/mapview.js
@@ -1,10 +1,11 @@
 /*
  * Client side javascript for displaying map of trucks
  */
-$(function() {
-    var foodTruckNS = foodTruckNS || {};
-    foodTruckNS.mapview = foodTruckNS.mapview || {};
 
+var foodTruckNS = foodTruckNS || {};
+foodTruckNS.mapview = foodTruckNS.mapview || {};
+
+$(function() {
     foodTruckNS.mapview.initialize = function() {
         var mapOptions = {
             center: new google.maps.LatLng(41.82, -71.40),
@@ -12,6 +13,25 @@ $(function() {
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+        /* add a test marker to the map */
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(41.82, -71.40),
+            map: map,
+            title: "Test"
+        });
+
+        /* add a test info window to the marker */
+        var infowindow = new google.maps.InfoWindow({
+            content: "PAPA JIMS HOMEY"
+        });
+
+        google.maps.event.addListener(marker, 'mouseover', function() {
+            infowindow.open(map,marker);
+        });
+        google.maps.event.addListener(marker, 'mouseout', function() {
+            infowindow.close(map,marker);
+        });
     };
 
     google.maps.event.addDomListener(window, 'load', foodTruckNS.mapview.initialize);
