edc1bb5451a297ea7c20328803035154b374f97a
Jackson Owens <jackson_owens@brown.edu>
2013-04-10 23:25:05 -0400

    success messages fade away

---
+++ src/public/js/global.js
@@ -0,0 +1,10 @@
+/*
+ * Client-side javascript included everywhere.
+ */
+var foodTruckNS = foodTruckNS || {};
+
+$(document).ready(function() {
+    setTimeout(function() {
+        $('.success-message').hide(1000);
+    }, 2000);
+});
