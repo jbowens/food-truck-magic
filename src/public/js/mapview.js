dd2ce9626b04f7c665515657c3077c6c948321d4
Alec Lee <alec.lee15@gmail.com>
2013-04-12 13:30:18 -0400

    integrated db with google maps

--- src/public/js/mapview.js
+++ src/public/js/mapview.js
@@ -5,7 +5,8 @@
 var foodTruckNS = foodTruckNS || {};
 foodTruckNS.mapview = foodTruckNS.mapview || {};
 
-foodTruckNS.mapview.initialize = function() {
+foodTruckNS.mapview.initmap = function() {
+    /* create the map near */
     var mapOptions = {
         center: new google.maps.LatLng(41.82, -71.40),
         zoom: 15,
@@ -13,28 +14,48 @@ foodTruckNS.mapview.initialize = function() {
     };
     var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-    /* add a test marker to the map */
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(41.82, -71.40),
-        map: map,
-        title: "Test"
-    });
+    /* add markers onto the map */
+    var geopoints = foodTruckNS.mapview.geopoints;
+    for (var i = 0; i < geopoints.length; i++) {
+        var geopoint = geopoints[i];
+        /* parse the "POINT(xx.xx yy.yy)" string */
+        var points = geopoint.point.split('(')[1];
+        points = points.substring(0, points.length-1).split(' ');
+        points[0] = parseFloat(points[0]);
+        points[1] = parseFloat(points[1]);
 
-    /* add a test info window to the marker */
-    var infowindow = new google.maps.InfoWindow({
-        content: "<a href='/trucks/papa-jims'>Papa Jim's</a>",
-        isOpen: false
-    });
+        /* actually add markers to the map */
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(points[0], points[1]),
+            map: map,
+            title: geopoint.name
+        });
 
+        var infowindow = new google.maps.InfoWindow({
+            content: "<a href='/trucks/" + geopoint.urlid + "'>" + geopoint.name + "</a>",
+            windowOpen: false
+        });
+        
+        foodTruckNS.mapview.attachClickHandler(marker, infowindow, map);
+    }
+};
+
+foodTruckNS.mapview.attachClickHandler = function(marker, infowindow, map) {
     google.maps.event.addListener(marker, 'click', function() {
-        if (infowindow.isOpen) {
+        if (infowindow.windowOpen) {
             infowindow.close(map, marker);
         } else {
             infowindow.open(map, marker);
         }
-        infowindow.isOpen = !infowindow.isOpen;
+        infowindow.windowOpen = !infowindow.windowOpen;
     });
+
 };
 
-
-google.maps.event.addDomListener(window, 'load', foodTruckNS.mapview.initialize);
+/*
+ * Where geopoints is an array of objects { point : STRING, urlid: STRING }
+ */
+foodTruckNS.mapview.init = function(geopoints) {
+    foodTruckNS.mapview.geopoints = geopoints;
+    google.maps.event.addDomListener(window, 'load', foodTruckNS.mapview.initmap);
+};
