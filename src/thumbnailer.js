46350f2f4e09e50413ca36160e4f7598fca3e867
Jackson Owens <jackson_owens@brown.edu>
2013-04-19 01:58:49 -0400

    worked on thumbnailer a lil

--- src/thumbnailer.js
+++ src/thumbnailer.js
@@ -9,26 +9,33 @@ var thumbnailSizes = [
         name: 'tiny-square',
         width: 60,
         height: 60,
-        crop: true
+        crop: true,
+        square: true
     },
     {
         name: 'small-square',
         width: 150,
         height: 150,
-        crop: true
+        crop: true,
+        square: true
     },
     {
         name: 'medium-square',
         width: 300,
         height: 300,
-        crop: true
+        crop: true,
+        square: true
     }
 ];
 
 exports.Thumbnailer = {
 
-    thumbnailify: function(photo) {
-
+    thumbnailify: function(photo, callback) {
+        var original = __dirname + '/../uploads/' + photo.id.toString() + photo.ext;
+        console.log("Generating thumbnails for", original);
+        for(var i = 0; i < thumbnailSizes.length; i++) {
+            /* Generate that shit */
+        }
     }
 
 };
