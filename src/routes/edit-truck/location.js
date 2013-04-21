72b0b3371a39c62602cef67b73d968cdd0c698b5
Alec Lee <alec.lee15@gmail.com>
2013-04-21 15:46:11 -0400

    hide the location page for non truck admins

--- src/routes/edit-truck/location.js
+++ src/routes/edit-truck/location.js
@@ -1,3 +1,15 @@
+var fourohfour = require('../fourohfour.js').route;
+
+function hasPermission(request, response, data) {
+    return !!(data.user && data.my_truck);
+}
+
 exports.route = function(request, response, data) {
+    if(!hasPermission(request, response, data)) {
+        /* The user must be logged in and the administrator for
+           a truck to view this page. */
+        return fourohfour(request, response, data);
+    }
+
     response.render('edit-truck-location', data);
 };
