5a45da5e9ebfe0bf34a6e58f4d2e860e1069b6ff
Alec Lee <alec.lee15@gmail.com>
2013-04-25 20:00:45 -0400

    so begins the horrendous journey that is query-trucks

--- src/routes/api/query-trucks.js
+++ src/routes/api/query-trucks.js
@@ -14,8 +14,52 @@ var SQL_GET_TRUCKS = 'SELECT trucks.*, trucks.photoUploadid AS uploadid, uploads
 
 var SQL_GET_FOLLOWED = 'SELECT trucks.* FROM follows INNER JOIN trucks on trucks.id = follows.truckid WHERE userid = $1';
 
+
+/*
+ * Appends the WHERE clause to the sql query.
+ * Not the prettiest thing in the world.
+ */
+var appendFilters = function(sql, body) {
+    args = [];
+    var firstFilter = true;
+
+    if ('open' in body) {
+        if (firstFilter) {
+            firstFilter = false;      
+            sql += " WHERE ";
+        } else {
+            sql += " AND ";
+        }
+        args.push(body.open);
+        sql += "trucks.open=$" + args.length;
+    }
+
+    if ('name' in body) {
+        if (firstFilter) {
+            firstFilter = false;      
+            sql += " WHERE ";
+        } else {
+            sql += " AND ";
+        }
+        args.push("%" + body.name + "%");
+        sql += "trucks.name ILIKE $" + args.length;
+    }
+    return [sql, args];
+};
+
+
+
+/*
+ * Expects request.body to have the following parameters:
+ * open - (optional) boolean specifying whether to query open/closed
+ * name - (optional) string specifying whether to do a search
+ */
 exports.postRoute = function(request, response, data) {
-    db.query(SQL_GET_TRUCKS, [], function(err, res) {
+    var tuple = appendFilters(SQL_GET_TRUCKS, request.body);
+    var sql = tuple[0];
+    var args = tuple[1];
+
+    db.query(sql, args, function(err, res) {
         if (err) {
             console.error(err);
             data.error = true;
