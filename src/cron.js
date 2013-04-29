fa03e49be8d5d02c503298ffed28cc8588e772d6
Alec Lee <alec.lee15@gmail.com>
2013-04-29 14:55:11 -0400

    moved around some less

--- src/cron.js
+++ src/cron.js
@@ -6,6 +6,7 @@ var db = require('./db.js').Database;
 var SQL_UPDATE_TO_CLOSE = 'UPDATE trucks SET open=false WHERE dateclose < now() AND open=true;';
 
 var autoCloseTrucks = function() {
+    console.log("Running cron for closing trucks");
     db.query(SQL_UPDATE_TO_CLOSE, [], function(err, res) {
         if (err) {
             console.error(err);
@@ -14,5 +15,6 @@ var autoCloseTrucks = function() {
 };
 
 exports.startCronJobs = function() {
+    autoCloseTrucks();
     setInterval(autoCloseTrucks, 5*60*1000); /* every 5 minutes */
 };
