665dff4aaa46d2d17a14c1ae60b7fda52bdf1151
Jackson Owens <jackson_owens@brown.edu>
2013-04-30 00:52:11 -0400

    :cat:egories.js

---
+++ src/categories.js
@@ -0,0 +1,101 @@
+/*
+ * Handles logic for dealing with categories.
+ */
+var db = require('./db.js').Database;
+
+/* SQL Queries */
+var SQL_GET_CATEGORIES = 'SELECT * FROM categories ORDER BY name ASC;';
+var SQL_GET_CLASSIFICATIONS = 'SELECT categories.* FROM classified_as ' +
+                                                  'INNER JOIN categories ' +
+                                                  'ON classified_as.catid = categories.catid ' +
+                                                  'WHERE classified_as.truckid = $1';
+var SQL_CLASSIFY_TRUCK = 'INSERT INTO classified_as (catid, truckid) VALUES($1, $2)';
+var SQL_DECLASSIFY_TRUCK = 'DELETE FROM classified_as WHERE catid = $1 AND truckid = $2';
+
+/*
+ * Gets all possible categories in the database.
+ */
+exports.getAllCategories = function(callback) {
+    db.query(SQL_GET_CATEGORIES, [], function(err, res) {
+        if(err) {
+            console.error(err);
+            callback(err, null);
+        } else {
+            callback(err, res.rows ? res.rows : []);
+        }
+    });
+};
+
+/*
+ * Gets all of the categories assigned to the given truckid.
+ */
+exports.getTrucksCategories = function(truckid, callback) {
+    db.query(SQL_GET_CLASSIFICATIONS, [truckid], function(err, res) {
+        if(err) {
+            console.error(err);
+            callback(err, null);
+        } else {
+            callback(err, res.rows ? res.rows : []);
+        }
+    });
+};
+
+/*
+ * Classifies the truck as the given category. The callback is
+ * optional.
+ */
+exports.classifyTruck = function(truckid, catid, callback) {
+    db.query(SQL_CLASSIFY_TRUCK, [catid, truckid], function(err, res) {
+        if(err) {
+            console.error(err);
+        }
+        if(callback) {
+            callback(err);
+        }
+    });
+};
+
+/*
+ * Deletes a classification for a particular truck. The callback is
+ * optional.
+ */
+exports.declassifyTruck = function(truckid, catid, callback) {
+    db.query(SQL_DECLASSIFY_TRUCK, [catid, truckid], function(err, res) {
+        if(err) {
+            console.error(err);
+        }
+        if(callback) {
+            callback(err);
+        }
+    });
+};
+
+/*
+ * Takes a truckid and a set of category ids and sets the truck to be in
+ * those categories and only those categories.
+ *
+ * TODO: Support a callback.
+ */
+exports.setClassifications = function(truckid, catids) {
+    exports.getTrucksCategories(truckid, function(err, cats) {
+        if(err) {
+            console.error(err);
+            return;
+        }
+        
+        var currentCatids = {};
+        for(var i = 0; i < cats.length; i++) {
+            currentCatids[cats.catid] = true;
+            if(!catids.indexOf(cats.catid)) {
+                exports.declassifyTruck(truckid, cats.catid);
+            }
+        }
+
+        for(i = 0; i < catids.length; i++) {
+            if(!currentCatids[catids[i]]) {
+                exports.classifyTruck(truckid, catids[i]);
+            }
+        }
+
+    });
+};
--- src/routes/edit-truck/info.js
+++ src/routes/edit-truck/info.js
@@ -1,12 +1,9 @@
 var _ = require('underscore');
 var check = require('validator').check;
 var sanitize = require('validator').sanitize;
-var db = require('../../db.js').Database;
+var categories = require('../../categories.js');
 var truckStore = require('../../truckstore.js').TruckStore;
 var fourohfour = require('./../fourohfour.js').route;
-
-/* SQL Queries */
-var SQL_GET_CATEGORIES = 'SELECT * FROM categories ORDER BY name ASC;';
 
 function renderPage(response, data) {
     response.render('edit-truck-info', data);
@@ -20,9 +17,9 @@ function hasPermission(request, response, data) {
  * Preloads the categories for the form.
  */
 exports.preloader = function(request, response, data, callback) {
-    db.query(SQL_GET_CATEGORIES, [], function(err, res) {
+    categories.getAllCategories(function(err, cats) {
         if(err) { console.error(err); }
-        data.categories = res.rows;
+        data.categories = cats;
         callback();
     });
 };
