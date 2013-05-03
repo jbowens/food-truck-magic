400938a2f86fbe1223972f3f72e602306a31ce85
Jackson Owens <jackson_owens@brown.edu>
2013-05-03 00:06:52 -0400

    truckle softly

--- src/public/js/truckle-box.js
+++ src/public/js/truckle-box.js
@@ -13,10 +13,43 @@ foodTruckNS.trucklebox.toggleTrucklebox = function(e) {
     e.preventDefault();
 };
 
+/* Called by constructMenu to construct menu items to populate
+ * the menu.
+ */
+foodTruckNS.trucklebox.constructMenuItem = function(url, name, className) {
+    var li = $('<li />');
+    if(className) {
+        $(li).addClass(className);
+    }
+    var a = $('<a />');
+    $(a).attr('href', url);
+    $(a).text(name);
+    $(li).append(a);
+    return li;
+};
+
+/* Constructs the truckle menu for the truckle box. God damn I love
+ * the name foodtruckler.
+ */
+foodTruckNS.trucklebox.constructMenu = function() {
+    var menu = $('<div id="truckle-menu" />');
+    
+    var list = $('<ul />');
+    $(menu).append(list);
+
+    $(list).append(foodTruckNS.trucklebox.constructMenuItem('/edit-truck/info', 'Manage your truck', 'edit-truck'));
+    $(list).append(foodTruckNS.trucklebox.constructMenuItem('/edit-truck/location', 'Update location', 'update-location'));
+    $(list).append(foodTruckNS.trucklebox.constructMenuItem('/edit-account/password', 'Edit your account', 'edit-account'));
+    $(list).append(foodTruckNS.trucklebox.constructMenuItem('/logout', 'Log out'));
+
+    return menu;
+};
+
 foodTruckNS.trucklebox.init = function() {
     if($("#truckle-box")) {
         /* Only initialize if the truckle box is on the page. */
         foodTruckNS.trucklebox.box = $("#truckle-box");
+        foodTruckNS.trucklebox.menu = foodTruckNS.trucklebox.constructMenu();
         $(foodTruckNS.trucklebox.box).click(foodTruckNS.trucklebox.toggleTrucklebox);
     }
 };
