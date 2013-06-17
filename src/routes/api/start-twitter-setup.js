c4c7d51d569d47949c69ad57527cebe76d06f6f8
Jackson Owens <jackson_owens@brown.edu>
2013-06-16 20:26:02 -0700

    twit request token shit

--- src/routes/api/start-twitter-setup.js
+++ src/routes/api/start-twitter-setup.js
@@ -5,8 +5,14 @@
  * /setup/twitter
  *
  */
-var twit = require('twit');
+var Twit = require('twit');
 var fourohfour = require('../fourohfour.js').route;
+
+// TODO: Update the callback to be correct.
+var TWITTER_OAUTH_TOKEN_CALLBACK = 'http://foodtruckler.com/api/access-token-response';
+
+// TODO: Properly initialize the Twit object.
+var twit = new Twit({});
 
 exports.postRoute = function(request, response, data) {
     if(!(data.user && data.my_truck)) {
@@ -15,8 +21,20 @@ exports.postRoute = function(request, response, data) {
         response.status(400);
         response.json({ err: true, errmsg: 'no twitter name' });
     } else {
-        /* Shit looks good. Try to request an auth token. */
-        /* TODO: Request an oauth token. */
-        response.json({ err: false });
+        /* Shit looks good. Try to request an oauth token. */
+        /* TODO: Save the twitter name. */
+        var requestParams = {
+            oauth_nonce: /* TODO: FILL ME IN! */null,
+            oauth_callback: TWITTER_OAUTH_TOKEN_CALLBACK
+        };
+        twit.post('oauth/request_token', {}, function (err, reply) {
+            if(err) {
+                response.json({ err: true, errmsg: err.toString() });
+            } else {
+                console.log("recieved oauth_token ", reply.oauth_token);
+                console.log("recieved oauth_token_secret ", reply.oauth_token_secret);
+                response.json({ err: false });
+            }
+        });
     }
 };
